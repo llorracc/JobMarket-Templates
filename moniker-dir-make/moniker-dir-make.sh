211a834c86908759e3e374b787724efd25e964cf
llorracc <git@llorracc.org>
2022-10-20 14:58:29 -0400

    moniker-dir-made -> moniker-dir

--- moniker-dir-make/moniker-dir-make.sh
+++ moniker-dir-make/moniker-dir-make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # For linux or macos machines
-# populates contents of moniker-dir-made directory
+# populates contents of moniker-dir directory
 # with, e.g., AbstractMoniker.pdf, CVMoniker.pdf, JobPaperMoniker.pdf
 
 # For windows machines you must either do it by hand or write a bat file
@@ -12,6 +12,6 @@ for file in *.pdf; do
     filename="${file%.*}"
     moniker="$(<.mymoniker)"
     outname="$filename""$moniker"
-    echo cp $filename.pdf moniker-dir-made/$filename/$outname.pdf
+    echo cp $filename.pdf moniker-dir/$filename/$outname.pdf
 done
 
